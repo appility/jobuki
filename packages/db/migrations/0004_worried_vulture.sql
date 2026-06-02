defc8e74a71b1f011781011c031703bdb6cee747
appility <appility@users.noreply.github.com>
2026-06-02 10:49:19 +0100

    updates

--- packages/db/migrations/0004_worried_vulture.sql
+++ packages/db/migrations/0004_worried_vulture.sql
@@ -4,5 +4,5 @@ EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 --> statement-breakpoint
-ALTER TABLE "boards" ADD COLUMN "board_config" jsonb;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "account_type" "account_type" DEFAULT 'board_creator' NOT NULL;
+ALTER TABLE "boards" ADD COLUMN IF NOT EXISTS "board_config" jsonb;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN IF NOT EXISTS "account_type" "account_type" DEFAULT 'board_creator' NOT NULL;
